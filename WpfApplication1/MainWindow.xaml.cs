4f87812c2c3476fd77e41565c1d32c28a5dac59c
music4santosh <34948354+music4santosh@users.noreply.github.com>
2017-12-29 17:51:59 -0500

    Update MainWindow.xaml.cs

--- WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+!﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -27,7 +27,7 @@ namespace WpfApplication1
 
 		private void Button_Click(object sender, RoutedEventArgs e)
 		{
-			MessageBox.Show("one tested");
+			MessageBox.Show("one tested!");
 		}
 	}
 }
